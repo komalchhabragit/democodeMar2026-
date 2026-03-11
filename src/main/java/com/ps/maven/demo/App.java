9a2fe63f5cb0d39290cb23b9adc438aa027c888d
komalchhabragit <52783974+komalchhabragit@users.noreply.github.com>
2026-03-11 11:41:22 +0530

    Commiting codebase

---
+++ src/main/java/com/ps/maven/demo/App.java
@@ -0,0 +1,13 @@
+package com.ps.maven.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/ps/maven/demo/AppTest.java
@@ -0,0 +1,23 @@
+package com.ps.maven.demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	System.out.println("target:" +System.getProperty("appurl"));
+       //assertTrue( true );
+    	 assertTrue( false );
+        
+    }
+}
